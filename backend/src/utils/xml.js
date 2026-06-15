2018fe6313c78e3d9ea9f040f4ba7cf1dcc66bef
Praveenramisetti76 <praveenramisetti76@gmail.com>
2026-06-15 16:04:52 +0530

    feat: add JSON to XML converter helper

---
+++ backend/src/utils/xml.js
@@ -0,0 +1,22 @@
+function jsonToXml(obj) {
+  let xml = '';
+  for (const prop in obj) {
+    xml += obj[prop] instanceof Array ? '' : "<" + prop + ">";
+    if (obj[prop] instanceof Array) {
+      for (const array in obj[prop]) {
+        xml += "<" + prop + ">";
+        xml += jsonToXml(new Object(obj[prop][array]));
+        xml += "</" + prop + ">";
+      }
+    } else if (typeof obj[prop] == "object") {
+      xml += jsonToXml(new Object(obj[prop]));
+    } else {
+      xml += obj[prop];
+    }
+    xml += obj[prop] instanceof Array ? '' : "</" + prop + ">";
+  }
+  xml = xml.replace(/\/g, '');
+  return xml;
+}
+
+module.exports = { jsonToXml };
