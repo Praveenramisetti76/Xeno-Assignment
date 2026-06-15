c1cb6dc968cadd3b0cdc26a531b8e6eff062ea9e
Praveenramisetti76 <praveenramisetti76@gmail.com>
2026-06-15 16:04:49 +0530

    feat: add currency formatter utility

---
+++ backend/src/utils/currency.js
@@ -0,0 +1,8 @@
+function formatCurrency(amount, currency = 'USD') {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency
+  }).format(amount);
+}
+
+module.exports = { formatCurrency };
