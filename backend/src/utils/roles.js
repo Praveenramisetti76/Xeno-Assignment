4f4b97fe9152bbe6278dc2128924622eb5074f0b
Praveenramisetti76 <praveenramisetti76@gmail.com>
2026-06-15 16:04:44 +0530

    feat: add user role checker middleware

---
+++ backend/src/utils/roles.js
@@ -0,0 +1,10 @@
+function restrictTo(...roles) {
+  return (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res.status(403).json({ error: 'You do not have permission' });
+    }
+    next();
+  };
+}
+
+module.exports = { restrictTo };
